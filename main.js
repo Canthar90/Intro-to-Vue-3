578e0e5ddb2b6c6b7610d7c1f58d6fe7e83e45a8
Canthar90 <d.cieslak90@gmail.com>
2023-03-13 16:31:57 +0100

    Basics of class bounding

--- main.js
+++ main.js
@@ -2,17 +2,30 @@
 const app = Vue.createApp({
     data(){
         return{
+            cart: 0,
             product: 'Socks',
-            image: "assets/images/socks_green.jpg",
+            image: "assets/images/socks_blue.jpg",
             url: "https://github.com/",
             inventory: 30,
             onSale: false,
+            inStock: false,
             details: ['50% cotton', "30% wool", "20% polyester"],
             variants: [
-                {id: 2234, color: "green"},
-                {id: 2235, color: "blue"},
+                {id: 2234, color: "green", image: "assets/images/socks_green.jpg"},
+                {id: 2235, color: "blue", image: "assets/images/socks_blue.jpg"},
             ],
             sizes: ['S', "M", "L", "XL"]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart ++
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        remFromCart(){
+            this.cart --
         }
     }
 });
